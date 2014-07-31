87c924befe07cabbb510c2a74f5ea54528e5332f
Matt Alexander <Matt.Alexander@massiveinteractive.com>
2014-07-31 12:32:03 +1000

    Initial setup

---
+++ src/Main.hx
@@ -0,0 +1,10 @@
+// Main.hx
+
+class Main
+{
+	public static function main()
+	{
+		var appView = new f1feed.app.view.AppView();
+		var appContext = new f1feed.app.context.AppContext(appView);
+	}
+}
